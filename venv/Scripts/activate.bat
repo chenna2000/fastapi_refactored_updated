592fc5bfee08b7e6b42aebc3d824149589abc92a
chenna200 <chennakesavarao95@gmail.com>
2025-09-18 12:09:17 +0530

    18 th september changes

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\asus\Downloads\exports\exports\exports\fastapi_refactored\venv
+set VIRTUAL_ENV=C:\Users\asus\Downloads\fastapi_refactored\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
